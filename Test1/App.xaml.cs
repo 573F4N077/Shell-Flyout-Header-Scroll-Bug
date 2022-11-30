f4b2061983f9bf57b9cc611264cd2c665da49239
Stefan Ott <stefan.ott@outlook.com>
2022-11-30 17:34:28 +0100

    Initial Upload

---
+++ Test1/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Test1;
+
+using Microsoft.Maui.Handlers;
+
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#elif __IOS__
+using UIKit;
+#endif
+
+public partial class App
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
+
+}
+
